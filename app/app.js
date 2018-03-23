fb03f7089e885efe796bd8efb36509e8fb0fd7b5
Jose Gonzalez <jgonz219@gmail.com>
2018-03-23 12:05:19 -0700

    added inquirer prompt

--- app/app.js
+++ app/app.js
@@ -8,14 +8,36 @@ const search = (username) => {
     git.search(username)
         .then(result => {
             console.log(`----Repos of ${username}----`)
-            let i = 1;
-            result.forEach(repo => {
-                console.log(i + '. ' + repo.name)
-                i++
-            });
+            // let i = 1;
+            // result.forEach(repo => {
+            //     console.log(i + '. ' + repo.name)
+            //     i++
+            // });
+            showRepo(result)
+            .then(result => {
+                console.log(result)
+            })
         })
         .catch(err => console.log(err))
 }
+
+const showRepo = (result) => {
+    return inquirer.prompt([{
+        type: 'list',
+        message: 'The Repo to search:',
+        name: 'Selecting Repo',
+        choices: result.map(function(repo) {
+            return `${repo.name}`
+        }),
+        validate: (answers) => { 
+            if (answers.length > 1) {
+                return false
+            }
+            else return true;
+        }
+    }])
+}
+
 
 module.exports = {
     search
