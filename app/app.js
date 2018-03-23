c30600aef9ab8f3275fd8988779c9d0cb82bc275
Unknown <alvinquach91@gmail.com>
2018-03-23 13:19:04 -0700

    cleaned up some stuff

--- app/app.js
+++ app/app.js
@@ -2,7 +2,6 @@ const
     git = require('./index.js'),
     inquirer = require('inquirer')
 
-
 const search = (username) => {
     git.search(username)
         .then(result => {
@@ -21,25 +20,22 @@ const showRepo = (result) => {
         choices: () => {
             let repos = []
             result.forEach(repo => {
-                repos.push({name: `${repo.name}`, value: `${repo.id}`})
+                repos.push({name: repo.name, value: repo})
             })
             return repos
         }
     }])
     .then (answer => {
-        result.forEach(repo => {
-            if (answer.repo == repo.id) {
-                console.log();
-                console.log(`------ Information for ${repo.name} ------`)
-                console.log(`Name: \t\t\t${repo.name}`)
-                console.log(`Created on: \t\t${repo.created_at}`)
-                console.log(`Owner: \t\t\t${repo.owner.login}`)
-                console.log(`Description: \t\t${repo.description}`)
-                console.log(`URL: \t\t\t${repo.html_url}`)
-                console.log(`Fork Count: \t\t${repo.forks_count}`)
-                console.log(`Repo Size: \t\t${repo.size} KB`)
-            }
-        })
+        const repo = answer.repo;
+        console.log();
+        console.log(`------ Information for ${repo.name} ------`)
+        console.log(`Name: \t\t\t${repo.name}`)
+        console.log(`Created on: \t\t${repo.created_at}`)
+        console.log(`Owner: \t\t\t${repo.owner.login}`)
+        console.log(`Description: \t\t${repo.description}`)
+        console.log(`URL: \t\t\t${repo.html_url}`)
+        console.log(`Fork Count: \t\t${repo.forks_count}`)
+        console.log(`Repo Size: \t\t${repo.size} KB`)
     })
     .catch(err => console.log(err))
 }
