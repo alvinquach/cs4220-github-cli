d28df9383b9e08b969c15808be59d43750f15c51
Jose Gonzalez <jgonz219@gmail.com>
2018-03-23 12:31:45 -0700

    added getting information of repo after choosing it

--- app/app.js
+++ app/app.js
@@ -8,15 +8,12 @@ const search = (username) => {
     git.search(username)
         .then(result => {
             console.log(`----Repos of ${username}----`)
-            // let i = 1;
-            // result.forEach(repo => {
-            //     console.log(i + '. ' + repo.name)
-            //     i++
+            //  let i = 1;
+            //  result.forEach(repo => {
+            //      console.log(i + '. ' + repo.name + " "  + repo.id)
+            //      i++
             // });
             showRepo(result)
-            .then(result => {
-                console.log(result)
-            })
         })
         .catch(err => console.log(err))
 }
@@ -25,19 +22,26 @@ const showRepo = (result) => {
     return inquirer.prompt([{
         type: 'list',
         message: 'The Repo to search:',
-        name: 'Selecting Repo',
-        choices: result.map(function(repo) {
-            return `${repo.name}`
-        }),
-        validate: (answers) => { 
-            if (answers.length > 1) {
-                return false
-            }
-            else return true;
+        name: 'repo',
+        choices: () => {
+            let repos = []
+            result.forEach(repo => {
+                repos.push({name: `${repo.name}`, value: `${repo.id}`})
+            })
+            return repos
         }
     }])
+    .then (answer => {
+        result.forEach(repo => {
+            if (answer.repo == repo.id) {
+                console.log(`------Information of ${repo.name}------`)
+                console.log(`Name: ${repo.name}`)
+                console.log(`Description: ${repo.description}`)
+            }
+        })
+    })
+    .catch(err => console.log(err))
 }
-
 
 module.exports = {
     search
